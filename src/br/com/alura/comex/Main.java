de1869a907acca5a42afb81828ec2240c4529bf9
Renan Mattos Silva <rmsilva@brq.com>
2022-07-25 19:25:12 -0300

    commit para teste inicial da semana 1

--- src/br/com/alura/comex/Main.java
+++ src/br/com/alura/comex/Main.java
@@ -3,6 +3,7 @@ package br.com.alura.comex;
 public class Main {
 
   public static void main(String[] args) {
+	  System.out.println("fazendo um teste para a branch semana 1");
     Pedido[] pedidos = ProcessadorDeCsv.processaArquivo("pedidos.csv");
   }
 }
