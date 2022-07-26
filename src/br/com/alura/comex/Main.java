78fa9b14089182b9083c530006b23d104dc09831
Renan Mattos Silva <msilvarenan@live.com>
2022-07-25 21:24:08 -0300

    Imprima informações do primeiro e do último pedidos

--- src/br/com/alura/comex/Main.java
+++ src/br/com/alura/comex/Main.java
@@ -9,6 +9,17 @@ public class Main {
 		//for (Pedido pedido : pedidos) {
 		//	System.out.println(pedido.getCliente());
 		//}
-		System.out.println("Numero de pedidos: " + pedidos.size());
+		Pedido primeiroPedido = pedidos.get(0);
+		System.out.println("Primeiro pedido: " + primeiroPedido);
+		Pedido ultimoPedido = pedidos.get(pedidos.size() -1);
+		System.out.println("Último pedido: " + ultimoPedido);
+		
+				
+		//System.out.println("Numero de pedidos: " + pedidos.size());
 	}
 }
+
+
+
+//Primeiro pedido: Pedido{categoria='INFORMÁTICA', produto='Notebook Samsung', cliente='ANA', preco=3523.00, quantidade=1, data=2022-01-01}`
+//Ultimo pedido: Pedido{categoria='INFORMÁTICA', produto='Galaxy Tab S8', cliente='BIA', preco=5939.10, quantidade=4, data=2022-01-02}
