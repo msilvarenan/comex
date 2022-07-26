097c9607921c04ddb17853995aa29ac9b1684cb0
Renan Mattos Silva <msilvarenan@live.com>
2022-07-25 21:31:41 -0300

    Lista com nomes dos clientes

--- src/br/com/alura/comex/Main.java
+++ src/br/com/alura/comex/Main.java
@@ -6,20 +6,23 @@ public class Main {
 	
 	public static void main(String[] args) {  
 		ArrayList<Pedido> pedidos = ProcessadorDeCsv.processaArquivo("pedidos.csv");
-		//for (Pedido pedido : pedidos) {
-		//	System.out.println(pedido.getCliente());
-		//}
+		
 		Pedido primeiroPedido = pedidos.get(0);
 		System.out.println("Primeiro pedido: " + primeiroPedido);
 		Pedido ultimoPedido = pedidos.get(pedidos.size() -1);
 		System.out.println("Último pedido: " + ultimoPedido);
 		
 				
-		//System.out.println("Numero de pedidos: " + pedidos.size());
+		ArrayList<String> listaClientes = new ArrayList<>();
+		for (Pedido pedido : pedidos) {
+			listaClientes.add(pedido.getCliente());
+		}
+		for (String cliente : listaClientes) {
+			System.out.println(cliente);
+			
+		}
 	}
 }
 
 
 
-//Primeiro pedido: Pedido{categoria='INFORMÁTICA', produto='Notebook Samsung', cliente='ANA', preco=3523.00, quantidade=1, data=2022-01-01}`
-//Ultimo pedido: Pedido{categoria='INFORMÁTICA', produto='Galaxy Tab S8', cliente='BIA', preco=5939.10, quantidade=4, data=2022-01-02}
