6771e836e8319c81be02300da60b7a41707242b4
Renan Mattos Silva <msilvarenan@live.com>
2022-07-28 20:35:36 -0300

    Lista ordenada de clientes sem duplicações

--- src/br/com/alura/comex/Main.java
+++ src/br/com/alura/comex/Main.java
@@ -8,35 +8,19 @@ public class Main {
 	
 	public static void main(String[] args) {  
 		ArrayList<Pedido> pedidos = ProcessadorDeCsv.processaArquivo("pedidos.csv");
-		
-		//Pedido primeiroPedido = pedidos.get(0);
-		//System.out.println("Primeiro pedido: " + primeiroPedido);
-		//Pedido ultimoPedido = pedidos.get(pedidos.size() -1);
-		//System.out.println("Último pedido: " + ultimoPedido);
-		
-				
-		ArrayList<String> listaCategoria = new ArrayList<>();
+						
+		ArrayList<String> listaCliente = new ArrayList<>();
 		for (Pedido pedido : pedidos) {
-			listaCategoria.add(pedido.getCliente());
+			listaCliente.add(pedido.getCliente());
 		}
-		//System.out.println(listaCategoria);
-		//for (String cliente : listaClientes) {
-		//	System.out.println(cliente);
-			
-		//}
 		
-		/*
-		 * var comparator = new PrecoPedidoComparator(); pedidos.sort(comparator);
-		 * 
-		 * for (Pedido pedido : pedidos) { System.out.println(pedido.getPreco()); }
-		 */
-		List<String> newList = listaCategoria.stream().distinct().collect(Collectors.toList());
-		for (String categoria : newList) {
-			System.out.println(categoria); 
+		List<String> novaLista = listaCliente.stream().distinct().sorted().collect(Collectors.toList());
+		for (String nome : novaLista) {
+			System.out.println(nome); 
 		}
+		
+		
 
-
-		//TestePullRequest
 	}
 }
 
