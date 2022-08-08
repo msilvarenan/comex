8b4a0cb83e59112077c633eb8deb25ca50033bbb
Renan Mattos Silva <msilvarenan@live.com>
2022-08-08 20:31:47 -0300

    todos os exercícios

--- src/br/com/alura/comex/Main.java
+++ src/br/com/alura/comex/Main.java
@@ -1,23 +1,49 @@
 package br.com.alura.comex;
 
-import java.util.Collection;
-import java.util.HashSet;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 public class Main {
 	
 	public static void main(String[] args) {  
 		List<Pedido> pedidos = ProcessadorDeCsv.processaArquivo("pedidos.csv");
-		//pedidos.stream().distinct().collect(Collectors.toList());
-		//List<Pedido> novaLista = pedidos.stream().distinct().sorted().collect(Collectors.toList());
-		//novaLista.forEach(pedido -> {System.out.println(pedido.getCategoria());});
-		
-		
+				
 		//Exercício 1: Lista de categorias sem duplicações
-		Collection<String> listaMutavel = new HashSet<>(); 
-		pedidos.forEach(pedido -> listaMutavel.add(pedido.getCategoria()));
-		listaMutavel.forEach(categoria -> {System.out.println(categoria);});
+		//Set<String> categoriasSemDuplicacao = new HashSet<>(); 
+		//pedidos.forEach(pedido -> categoriasSemDuplicacao.add(pedido.getCategoria()));
+		//categoriasSemDuplicacao.forEach(categoria -> {System.out.println(categoria);});
 			
+		//Exercício 2
+		
+		Map<String, Integer> numeroPedidosPorCategoria = new HashMap<>();
+		for (Pedido pedido : pedidos) {
+			String categoria = pedido.getCategoria();
+			
+			Integer quantidade = numeroPedidosPorCategoria.get(categoria);
+			if(quantidade != null) {
+				numeroPedidosPorCategoria.put(categoria, quantidade + 1);
+			}else {
+				numeroPedidosPorCategoria.put(categoria, 1);
+			}
+		}
+		for (String categoria : numeroPedidosPorCategoria.keySet()) {
+			Integer quantidadePorCategoria = numeroPedidosPorCategoria.get(categoria);
+			System.out.println(categoria +": "+ quantidadePorCategoria);
+		}
+		
+		//Exemplo do Douglas >
+		//pedidos.stream().collect(Collectors.groupingBy(Pedido::getCategoria, Collectors.counting())));
+		
+					
+		//Exercício 3: Lista ordenada de clientes sem duplicações
+		//TreeSet<String> clientesSemDuplicacaoOrdenado = new TreeSet<>(new NomeComparator());
+		//pedidos.forEach(pedido -> clientesSemDuplicacaoOrdenado.add(pedido.getCliente()));
+		//clientesSemDuplicacaoOrdenado.forEach(cliente -> {System.out.println(cliente);});
+		
+		//Exemplo Exercício 3 >
+		//pedidos.stream().map(Pedido::getCliente).collect(Collectors.toCollection(TreeSet::new)).forEach(System.out::println);
 	}
 }
 
